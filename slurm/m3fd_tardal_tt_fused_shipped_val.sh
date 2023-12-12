621a1f6fa83f6516f47b9940c14c069254d448b5
PIEthonista <gohyixian456@gmail.com>
2023-12-12 20:04:11 +0900

    twcc

--- slurm/m3fd_tardal_tt_fused_shipped_val.sh
+++ slurm/m3fd_tardal_tt_fused_shipped_val.sh
@@ -9,6 +9,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 549844
-srun python val.py --img 320 --batch 16 --weights /work/u5832291/yixian/yolov5_edit/runs/train/YOLOv5s_m3fd_tardal_tt_fused_shipped_20231211/weights/best.pt --data m3fd_tardal_tt_fused_shipped.yaml --device 0 --verbose --task val --conf-thres 0.001 --save-txt --save-conf --name YOLOv5s_m3fd_tardal_tt_fused_shipped_20231211
+# # 549844
+# srun python val.py --img 320 --batch 16 --weights /work/u5832291/yixian/yolov5_edit/runs/train/YOLOv5s_m3fd_tardal_tt_fused_shipped_20231211/weights/best.pt --data m3fd_tardal_tt_fused_shipped.yaml --device 0 --verbose --task val --conf-thres 0.001 --save-txt --save-conf --name YOLOv5s_m3fd_tardal_tt_fused_shipped_20231211
+
+srun python val.py --img 320 --batch 16 --weights /work/u5832291/yixian/yolov5_edit/runs/train/YOLOv5X_m3fd_tardal_tt_fused_shipped_20231212/weights/best.pt --data m3fd_tardal_tt_fused_shipped.yaml --device 0 --verbose --task val --conf-thres 0.001 --save-txt --save-conf --name YOLOv5X_m3fd_tardal_tt_fused_shipped_20231212
 
